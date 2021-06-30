8b11cb27d5906263b513c8a9c8f6f5875c40be77
andre <andre@DESKTOP-PMTSH1N.lan>
2021-06-30 20:00:34 +0200

    dbconnect modificato con hikari

--- Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/dao/ConnectDB.java
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/dao/ConnectDB.java
@@ -1,17 +1,33 @@
 package it.polito.tdp.anagrammi.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+//import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 public class ConnectDB {
 	
 	static private final String jdbcUrl = "jdbc:mysql://localhost/dizionario?user=root&password=root";
 
+	static private HikariDataSource ds = null;
+	
 	public static Connection getConnection() {
-
+		
+		if(ds==null) {
+		ds = new HikariDataSource();
+		
+		ds.setJdbcUrl(jdbcUrl);
+		
+		// non servono SE le abbiamo gia messe nel jdbcUrl sopra
+		// ds.setUsername("root");
+		// ds.setPassword("root");
+		}
+		
 		try {
-				Connection connection = DriverManager.getConnection(jdbcUrl);
+				// usiamo hikari per connetterci a database
+				Connection connection = ds.getConnection();
+				//Connection connection = DriverManager.getConnection(jdbcUrl);
 				return connection;
 
 		} catch (SQLException e) {
